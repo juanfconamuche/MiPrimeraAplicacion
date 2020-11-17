f537418671104383d1c8e2567a7a272bd69d50ea
juanfconamuche <74580443+juanfconamuche@users.noreply.github.com>
2020-11-16 23:19:43 -0500

    Initial commit

---
+++ app/src/main/java/com/francisco/miprimeraaplicacion/AcercaDe.java
@@ -0,0 +1,16 @@
+package com.francisco.miprimeraaplicacion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AcercaDe extends Activity
+ {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acerca_de);
+    }
+}
---
+++ app/src/main/java/com/francisco/miprimeraaplicacion/InterfaceToast.java
@@ -0,0 +1,5 @@
+package com.francisco.miprimeraaplicacion;
+
+public interface InterfaceToast {
+    void mensaje(String mensaje);
+}
